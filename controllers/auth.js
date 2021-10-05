8857e4166fefe02afea25f097e63b3ef8d8efe15
Ilder Velez <velez.ilder@gmail.com>
2021-10-04 21:48:43 -0400

    Fix signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,15 +3,11 @@ const validator = require('validator')
 const User = require('../models/User')
 
 exports.login = (req, res, next) => {
-	const validationErrors = []
 	if (!validator.isEmail(req.body.email))
-		validationErrors.push({ msg: 'Please enter a valid email address.' })
+		res.json({ msg: 'Please enter a valid email address.' })
 	if (validator.isEmpty(req.body.password))
-		validationErrors.push({ msg: 'Password cannot be blank.' })
+		res.json({ msg: 'Password cannot be blank.' })
 
-	if (validationErrors.length) {
-		req.flash('errors', validationErrors)
-	}
 	req.body.email = validator.normalizeEmail(req.body.email, {
 		gmail_remove_dots: false,
 	})
@@ -21,13 +17,13 @@ exports.login = (req, res, next) => {
 			return next(err)
 		}
 		if (!user) {
-			req.flash('errors', info)
+			res.json({ msg: 'User not found.' })
 		}
 		req.logIn(user, (err) => {
 			if (err) {
 				return next(err)
 			}
-			req.flash('success', { msg: 'Success! You are logged in.' })
+			res.json({ msg: 'Success! You are logged in.' })
 		})
 	})(req, res, next)
 }
@@ -36,25 +32,30 @@ exports.logout = (req, res) => {
 	req.logout()
 	req.session.destroy((err) => {
 		if (err)
-			console.log('Error : Failed to destroy the session during logout.', err)
+			res.json({ msg: 'Error : Failed to destroy the session during logout.' })
 		req.user = null
 	})
+	res.status(200)
 }
 
 exports.signup = (req, res, next) => {
 	const validationErrors = []
+
 	if (!validator.isEmail(req.body.email))
 		validationErrors.push({ msg: 'Please enter a valid email address.' })
+
 	if (!validator.isLength(req.body.password, { min: 8 }))
 		validationErrors.push({
 			msg: 'Password must be at least 8 characters long',
 		})
+
 	if (req.body.password !== req.body.confirmPassword)
 		validationErrors.push({ msg: 'Passwords do not match' })
 
 	if (validationErrors.length) {
 		req.flash('errors', validationErrors)
 	}
+
 	req.body.email = validator.normalizeEmail(req.body.email, {
 		gmail_remove_dots: false,
 	})
@@ -72,7 +73,7 @@ exports.signup = (req, res, next) => {
 				return next(err)
 			}
 			if (existingUser) {
-				req.flash('errors', {
+				res.json({
 					msg: 'Account with that email address or username already exists.',
 				})
 			}
@@ -84,6 +85,7 @@ exports.signup = (req, res, next) => {
 					if (err) {
 						return next(err)
 					}
+					res.json({ msg: 'Signup successful' })
 				})
 			})
 		}
